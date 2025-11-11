3c6e2ce49b2acc3b24970480366d1aabb8e1e24a
laurasg0 <laaurasg.03@gmail.com>
2025-11-11 17:39:49 +0100

    notas.txt

--- users/models.py
+++ users/models.py
@@ -3,13 +3,18 @@ from django.utils.timezone import now
 
 # Create your models here.
 class Users(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Name', null=False, blank=False)
-    surname = models.CharField(max_length=150, verbose_name='Surname', null=False, blank=False)
-    email = models.CharField()
-    phone = models.IntegerField(max_length=9, verbose_name='Phone')
-    password = models.CharField(max_length=50, verbose_name='Password', null=False, blank=False)
+    name = models.CharField(max_length=50, verbose_name='Nombre', null=False, blank=False)
+    surname = models.CharField(max_length=150, verbose_name='Apellidos', null=False, blank=False)
+    email = models.CharField(max_length=250, verbose_name='Correo Electrónico', null=False, blank=False)
+    phone = models.IntegerField(max_length=9, verbose_name='Teléfono')
+    password = models.CharField(max_length=50, verbose_name='Contraseña', null=False, blank=False)
     Rol = models.OneToOneField(Rol, verbose_name='Rol', related_name='roles')
-    register_date = models.DateTimeField(verbose_name = 'Register date', default=now)
+    register_date = models.DateTimeField(verbose_name = 'Fecha de Registro', default=now)
 
 class Rol(models.Model):
-    name = models.VharFIeld(max_length = 50, verbose_name = 'Name')
+    name = models.CharField(max_length = 50, verbose_name = 'Name')
+
+
+# campo DateTimeField para guardar la última conexión
+
+# crear app de session
