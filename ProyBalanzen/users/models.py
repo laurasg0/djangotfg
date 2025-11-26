770d55929536759e53f804a633072f9f22ec710b
laurasg0 <laaurasg.03@gmail.com>
2025-11-26 15:37:05 +0100

    mover proyecto a carpeta ProyBalanzen para la posterior creación de Dockerfile

--- ProyBalanzen/users/models.py
+++ ProyBalanzen/users/models.py
@@ -24,21 +24,24 @@ class Users(models.Model):
     surname = models.CharField(max_length=150, verbose_name='Apellidos', null=False, blank=False)
     email = models.EmailField(max_length=250, verbose_name='Correo Electrónico', null=False, blank=False)
     password = models.CharField(max_length=50, verbose_name='Contraseña', null=False, blank=False)
-    phone = models.CharField(verbose_name='Teléfono')
+    phone = models.CharField(verbose_name='Teléfono', null=False, blank=False)
     birthdate = models.DateTimeField(verbose_name= 'Fecha de Nacimiento', blank=True, default=None)
     gender = models.CharField(max_length=10, choices=Gender.choices, verbose_name='Género', default=None)
     role = models.CharField(max_length=10, choices=Role.choices, verbose_name='Rol', default=Role.CLIENTE)
-    register_date = models.DateTimeField(verbose_name='Fecha de Registro', default=now)
-    reward_points = models.IntegerField(verbose_name='Balanzen Points', default=0)
-    membership_level = models.CharField(max_length=10, choices=Membership.choices, default=Membership.PLATA)
+    register_date = models.DateTimeField(verbose_name='Fecha de Registro', default=now, null=False, blank=False)
+    reward_points = models.IntegerField(verbose_name='Balanzen Points', default=0, null=False, blank=False)
+    membership_level = models.CharField(max_length=10, choices=Membership.choices, default=Membership.PLATA, null=False, blank=False)
     
     def __str__(self):
         return f"{self.name} {self.surname}"
     
+# 1:M Un usuario puede tener una o más sesiones 
+
 class Session(models.Model):
     token_session = models.CharField(max_length=255, unique=True)
     start_date = models.DateTimeField(default=now)
     end_date = models.DateTimeField(null=True, blank=True)
+    id_user = models.ForeignKey()
 
 
 # campo DateTimeField para guardar la última conexión
