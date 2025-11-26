a7879c708a52809af975dc2f081a3be5b6f226ec
laurasg0 <laaurasg.03@gmail.com>
2025-11-26 16:26:08 +0100

    avance modelos

--- ProyBalanzen/classes/models.py
+++ ProyBalanzen/classes/models.py
@@ -6,3 +6,23 @@ class Classes(models.Model):
     name = models.CharField(max_length=50, verbose_name='Clase', null=False, blank=False)
     description = models.CharField(max_length=150, verbose_name='Descripción', null=False, blank=False)
     price = models.FloatField(verbose_name='Precio', null=False, blank=False)
+    # TODO: ID_CENTER FK
+
+    def __str__(self):
+        return self.name
+    
+# Modelo Turn (Reserva de clase por usuario)
+class Turn(models.Model):
+    date = models.DateField(verbose_name='Fecha', null=False, blank=False)
+    time = models.TimeField(verbose_name='Hora', null=False, blank=False)
+    capacity = models.IntegerField(verbose_name='Capacidad', null=False, blank=False)
+    id_user = models.ForeignKey(
+        'users.User',
+        on_delete=models.CASCADE,
+        related_name='turns'
+    )
+    id_class = models.ForeignKey(
+        Classes,
+        on_delete=models.CASCADE,
+        related_name='turns'
+    )
--- ProyBalanzen/users/models.py
+++ ProyBalanzen/users/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 from django.utils.timezone import now
 
-# Create your models here.
-class Users(models.Model):
+# TODO: cambiar y extender de AbstractUser
+class User(models.Model):
 
     class Gender(models.TextChoices):
         MUJER = 'MUJER', 'Mujer'
-        HOMBRE = 'Hombre', 'Hombre'
+        HOMBRE = 'HOMBRE', 'Hombre'
         OTRO = 'OTRO', 'Otro'
     
     class Role(models.TextChoices):
@@ -22,28 +22,30 @@ class Users(models.Model):
     
     name = models.CharField(max_length=50, verbose_name='Nombre', null=False, blank=False)
     surname = models.CharField(max_length=150, verbose_name='Apellidos', null=False, blank=False)
-    email = models.EmailField(max_length=250, verbose_name='Correo Electrónico', null=False, blank=False)
+    email = models.EmailField(max_length=300, verbose_name='Correo Electrónico', null=False, blank=False, unique=True)
     password = models.CharField(max_length=50, verbose_name='Contraseña', null=False, blank=False)
-    phone = models.CharField(verbose_name='Teléfono', null=False, blank=False)
-    birthdate = models.DateTimeField(verbose_name= 'Fecha de Nacimiento', blank=True, default=None)
-    gender = models.CharField(max_length=10, choices=Gender.choices, verbose_name='Género', default=None)
+    phone = models.CharField(max_length=20, verbose_name='Teléfono', null=False, blank=False)
+    birthdate = models.DateField(verbose_name= 'Fecha de Nacimiento', blank=True, null = True, default=None)
+    gender = models.CharField(max_length=10, choices=Gender.choices, verbose_name='Género', default=None, null = True)
     role = models.CharField(max_length=10, choices=Role.choices, verbose_name='Rol', default=Role.CLIENTE)
     register_date = models.DateTimeField(verbose_name='Fecha de Registro', default=now, null=False, blank=False)
     reward_points = models.IntegerField(verbose_name='Balanzen Points', default=0, null=False, blank=False)
     membership_level = models.CharField(max_length=10, choices=Membership.choices, default=Membership.PLATA, null=False, blank=False)
+    last_connection = models.DateTimeField(null=True, blank=True)
     
     def __str__(self):
         return f"{self.name} {self.surname}"
     
 # 1:M Un usuario puede tener una o más sesiones 
 
-class Session(models.Model):
+class UserSession(models.Model):
     token_session = models.CharField(max_length=255, unique=True)
     start_date = models.DateTimeField(default=now)
     end_date = models.DateTimeField(null=True, blank=True)
-    id_user = models.ForeignKey()
-
-
-# campo DateTimeField para guardar la última conexión
+    id_user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='sessions'
+    )
 
 # crear app de session1
