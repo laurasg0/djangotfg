657b2c1fb4fbf65e75737a7ec7a7086eb6521522
laurasg0 <laaurasg.03@gmail.com>
2025-12-03 14:55:55 +0100

    .

--- ProyBalanzen/ads/models.py
+++ ProyBalanzen/ads/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+# modelo anuncios para administrar los anuncios en la plataforma
+class Anuncio(models.Model):
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    fecha_publicacion = models.DateTimeField(auto_now_add=True)
+    activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.titulo
